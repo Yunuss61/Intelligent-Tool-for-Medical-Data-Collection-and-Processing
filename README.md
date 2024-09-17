e602eb3e457df6dc6ab6f5e773ae3944f1c359c1
Yunuss61 <muhammedinusa18@gmail.com>
2024-09-17 17:08:59 +0100

    Add files via upload

---
+++ README.md
@@ -0,0 +1,25 @@
+# Medical-Predictions-Streamlit-Web-App
+
+This app is used to predict the medical state of an individual
+The disease sections include ->
+1. Diabetes
+2. Heart Disease 
+
+
+Each prediction page is conneceted with a Machine Learning Model which uses Random Forest Classifier to predict the results.
+Also we have 3 different datasets used for each prediction.
+We can land into each prediction site of the web app from the options in the Navigation Menu.
+
+
+Each prediction is done with the help of 4 features which will be taken as input from the user.
+The most relevant features are taken into consideration for prediction also these features can be found out with simple tests or analysis without visiting any doctor.
+So the victim can get a broad overview of their health condition.
+The features taken into consideration are as follows=
+
+1. Diabetes = glucose, insulin, Body Mass Index(BMI), age
+2. Heart Disease = chest pain, Blood Pressure(BP), cholestrol, max Heart Rate(HR)
+
+The feature selection is carefully done under the supervision of a medical science student.
+
+
+After the modeling part the model is deployed using Streamlit library on Streamlit Share so that the app is available for usage for everyone.
---
+++ app.py
@@ -0,0 +1,86 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+import plotly.express as px
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+
+rad=st.sidebar.radio("Navigation Menu",["Home","Diabetes Section","Heart Disease Section"])
+
+#Home Page
+if rad=="Home":
+    st.title("Intelligent Tool for Medical Data Collection and Processing")
+    # st.image("Medical Prediction Home Page.jpg")
+    st.text("Project By:")
+    st.text("Supervised By:")
+    st.text("The Following Diseases are Predicted based on Medical information provided->")
+    
+    st.text("1. Diabetes Predictions")
+    st.text("2. Heart Disease Predictions")
+
+
+#Diabetes Prediction
+df2=pd.read_csv("Diabetes Predictions.csv")
+x2=df2.iloc[:,[1,4,5,7]].values
+x2=np.array(x2)
+y2=y2=df2.iloc[:,[-1]].values
+y2=np.array(y2)
+x2_train,x2_test,y2_train,y2_test=train_test_split(x2,y2,test_size=0.2,random_state=0)
+model2=RandomForestClassifier()
+model2.fit(x2_train,y2_train)
+
+#Diabetes Page
+if rad=="Diabetes Section":
+    st.header("Measure risk of Diabetes for Patient")
+    st.write("All The Values Should Be In Range Mentioned")
+    glucose=st.number_input("Enter Your Glucose Level (0-200)",min_value=0,max_value=200,step=1)
+    insulin=st.number_input("Enter Your Insulin Level In Body (0-850)",min_value=0,max_value=850,step=1)
+    bmi=st.number_input("Enter Your Body Mass Index/BMI Value (0-70)",min_value=0,max_value=70,step=1)
+    age=st.number_input("Enter Your Age (20-80)",min_value=20,max_value=80,step=1)
+    prediction2=model2.predict([[glucose,insulin,bmi,age]])[0]
+
+    if st.button("Predict"):
+        if prediction2==1:
+            st.warning("You Might Be Affected By Diabetes")
+        elif prediction2==0:
+            st.success("You Are Safe")
+
+#Heart Disease Prediction
+df3=pd.read_csv("Heart Disease Predictions.csv")
+x3=df3.iloc[:,[2,3,4,7]].values
+x3=np.array(x3)
+y3=y3=df3.iloc[:,[-1]].values
+y3=np.array(y3)
+x3_train,x3_test,y3_train,y3_test=train_test_split(x3,y3,test_size=0.2,random_state=0)
+model3=RandomForestClassifier()
+model3.fit(x3_train,y3_train)
+
+#Heart Disease Page
+if rad=="Heart Disease Section":
+    st.header("Measure risk of Heart Disease for Patient")
+    st.write("All The Values Should Be In Range Mentioned")
+    chestpain=st.number_input("Rate Your Chest Pain (1-4)",min_value=1,max_value=4,step=1)
+    bp=st.number_input("Enter Your Blood Pressure Rate (95-200)",min_value=95,max_value=200,step=1)
+    cholestrol=st.number_input("Enter Your Cholestrol Level Value (125-565)",min_value=125,max_value=565,step=1)
+    maxhr=st.number_input("Enter You Maximum Heart Rate (70-200)",min_value=70,max_value=200,step=1)
+    prediction3=model3.predict([[chestpain,bp,cholestrol,maxhr]])[0]
+
+    if st.button("Predict"):
+        if str(prediction3)=="Presence":
+            st.warning("You Might Be Affected By Heart Disease")
+        elif str(prediction3)=="Absence":
+            st.success("You Are Safe")
+                                        
+                                        
+if rad=="Plots":
+    type=st.selectbox("Which Plot Do You Want To See?",["Covid-19","Diabetes","Heart Disease"])
+    if type=="Covid-19":
+        fig=px.scatter(df1,x="Difficulty in breathing",y="Infected with Covid19")
+        st.plotly_chart(fig)
+
+    elif type=="Diabetes":
+        fig=px.scatter(df2,x="Glucose",y="Outcome")
+        st.plotly_chart(fig)
+    elif type=="Heart Disease":
+        fig=px.scatter(df3,x="BP",y="Heart Disease")
+        st.plotly_chart(fig)
